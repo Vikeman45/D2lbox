997d59e8a047ce09ce47a87632d75e5f980d417a
Mike VanGeel <mtvangeel@gmail.com>
2025-06-19 20:36:56 -0500

    Commented out non-functional code to allow initial push to Github Pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { useUserProfileStore } from '@/stores/userProfileStore'
-import { useAppDatabaseStore } from '@/stores/appDatabaseStore'
+// import { useAppDatabaseStore } from '@/stores/appDatabaseStore'
 
 const store = useUserProfileStore()
-const db = useAppDatabaseStore()
+// const db = useAppDatabaseStore()
 </script>
 
 <template>
   <main class="home app-grid-main">
     <h1>Welcome {{ store.userName.length > 0 ? `Back ${store.userName}` : 'Agent' }}!</h1>
-    <p>App Database State : {{ db.dbStatus }}</p>
+    <!-- <p>App Database State : {{ db.dbStatus }}</p>
     <p>Open: {{ db.dbIsOpen }}</p>
-    <p>May Be Stale: {{ db.dbMayBeStale }}</p>
+    <p>May Be Stale: {{ db.dbMayBeStale }}</p> -->
   </main>
 </template>
